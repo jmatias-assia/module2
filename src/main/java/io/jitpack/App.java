611eef96425b929fba451ddec73877916e585a83
Jorge Matias <jmatias@assia-inc.com>
2021-08-04 16:30:18 +0200

    first commit

---
+++ src/main/java/io/jitpack/App.java
@@ -0,0 +1,15 @@
+package io.jitpack;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static String GREETING = "Hello World 3!";
+    
+    public static void main( String[] args )
+    {
+        System.out.println( GREETING );
+    }
+}
